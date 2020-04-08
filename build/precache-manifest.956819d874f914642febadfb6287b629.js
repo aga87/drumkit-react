615fa1ea9306a6f77ba4c6c4bcedc5c8420e54b6
Aga <aga.labonarska@outlook.com>
2020-04-08 12:50:51 +0100

    build

---
+++ build/precache-manifest.956819d874f914642febadfb6287b629.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc6cb14c77a981072978302a413294bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fba68fe50d32032b1e8",
+    "url": "/static/css/main.75fedab6.chunk.css"
+  },
+  {
+    "revision": "efb875e0200879e27378",
+    "url": "/static/js/2.5a071058.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5a071058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fba68fe50d32032b1e8",
+    "url": "/static/js/main.1055e5e8.chunk.js"
+  },
+  {
+    "revision": "26e263e10a196582abd5",
+    "url": "/static/js/runtime-main.692cf7e9.js"
+  },
+  {
+    "revision": "90d3fb4f6bd67e57e683c75174d87d0a",
+    "url": "/static/media/bass.90d3fb4f.wav"
+  },
+  {
+    "revision": "b6b4c424e94d5604d507c07112d4ba6a",
+    "url": "/static/media/chip1.b6b4c424.wav"
+  },
+  {
+    "revision": "094fa7b873a1e995bde116b9ac5dfcbb",
+    "url": "/static/media/chip2.094fa7b8.wav"
+  },
+  {
+    "revision": "f53e933e3d3dad0570d926521b79e9f4",
+    "url": "/static/media/hihat.f53e933e.wav"
+  },
+  {
+    "revision": "62b3e5302818ee2e10f4cac41cc3f836",
+    "url": "/static/media/kick.62b3e530.wav"
+  },
+  {
+    "revision": "64be3114ea7100dc67fa5a4ac4286c49",
+    "url": "/static/media/notes-grey.64be3114.svg"
+  },
+  {
+    "revision": "3533fbf6fd3e9a42da8a530a71f8612a",
+    "url": "/static/media/snare.3533fbf6.wav"
+  },
+  {
+    "revision": "5f613fb2446adf358d5c95bd2afccb41",
+    "url": "/static/media/tom1.5f613fb2.wav"
+  },
+  {
+    "revision": "6285f528689e7aa053aa0f2bc863657b",
+    "url": "/static/media/tom2.6285f528.wav"
+  }
+]);
